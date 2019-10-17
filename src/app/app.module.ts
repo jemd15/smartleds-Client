5ab061c1bef9790e4afcdf6ea75b91e75f36d60a
Javier Muñoz <x_jemd_x@hotmail.com>
2019-10-17 11:53:44 -0300

    eliminando materializecss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { MaterializeModule } from "angular2-materialize";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
